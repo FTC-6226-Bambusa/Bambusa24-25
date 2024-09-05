c60708e870c35f71333f1813fe4a2c6e6d44eab6
FTC-6226-Bambusa <122700201+FTC-6226-Bambusa@users.noreply.github.com>
2024-09-05 16:51:12 -0700

    Main commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerSetup.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.TrajectorySegment;
+
+public class RoadRunnerSetup extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+       // Trajectory something = new TrajectoryBuilder()
+        while(!isStopRequested())
+        {
+
+        }
+    }
+}
